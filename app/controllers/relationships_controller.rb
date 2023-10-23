b673e60f8a2d91f7cc6c609aee11670daacd8f40
Netlooo <130125070+Netlooo@users.noreply.github.com>
2023-10-23 20:19:00 +0000

    [Add] User モデル, Relationshipコントローラ,　ルーティング

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,8 +1,33 @@
 class RelationshipsController < ApplicationController
 
   def create
+     @user = User.find(params[:user_id])
+    current_user.follow(params[:user_id])
+
+    @user.create_notification_follow!(current_user)
+    redirect_to request.referer
   end
 
   def destroy
+    current_user.unfollow(params[:user_id])
+    redirect_to request.referer
+  end
+
+  def followings
+    user = User.find(params[:user_id])
+    @users = user.followings
+  end
+
+  def followers
+    user = User.find(params[:user_id])
+    @users = user.followers
+  end
+
+  def follow(user_id)
+    favorites.create(follow_id: user_id)
+  end
+
+  def unfollow(user_id)
+    favorites.find_by(follow_id: user_id).destroy
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,12 @@ class User < ApplicationRecord
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+
+  def follow(user_id)
+    favorites.create(follow_id: user_id)
+  end
+
+  def unfollow(user_id)
+    favorites.find_by(follow_id: user_id).destroy
+  end
 end
