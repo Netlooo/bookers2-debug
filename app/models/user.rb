7929af3d799be63bfba35ecb1acfe0357a1593c5
Netlooo <130125070+Netlooo@users.noreply.github.com>
2024-03-07 14:31:35 +0000

    [fix] user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,13 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-
+  has_many :relationships, dependent: :destroy
 
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followings, through: :relationships, source: :follower
 
-  has_many :reverse_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :followers, through: :reverse_relationships, source: :followed
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :followed
 
   has_one_attached :profile_image
 
@@ -21,12 +21,8 @@ class User < ApplicationRecord
   validates :introduction, length: { maximum: 50 }
 
 
-  def get_profile_image(width, height)
-    unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    end
-    profile_image.variant(resize_to_limit: [width, height]).processed
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
 
   def follow(user_id)
@@ -35,5 +31,9 @@ class User < ApplicationRecord
 
   def unfollow(user_id)
     favorites.find_by(follow_id: user_id).destroy
+  end
+
+  def following?(user)
+  followings.include?(user)
   end
 end
